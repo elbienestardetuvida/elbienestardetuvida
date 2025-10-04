5779c9ed872a1ac11104197b7155e829bf973b03
brianGelvez <briannn97@gmail.com>
2025-10-04 02:23:09 -0300

    comentando componente debug

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
           <CartProvider>
             {children}
             <OffersSystem />
-            <OffersDebug />
+            {/* <OffersDebug /> */}
           </CartProvider>
         </ToastProvider>
       </body>
